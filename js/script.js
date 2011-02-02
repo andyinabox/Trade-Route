e103f738b4af60e1ecd2e070ff421c6b244e6ac9
amdayton <amdayton@gmail.com>
2011-02-02 16:03:18 -0500

    Added Tiles@Home OpenStreetMap map

--- js/script.js
+++ js/script.js
@@ -6,8 +6,11 @@ var map;
 		var ol_wms = new OpenLayers.Layer.WMS( "OpenLayers WMS", "http://vmap0.tiles.osgeo.org/wms/vmap0", {layers: 'basic'} );
 		// var jpl_wms = new OpenLayers.Layer.WMS( "NASA Global Mosaic", "http://t1.hypercube.telascience.org/cgi-bin/landsat7", {layers: "landsat7"});
 		// var dm_wms = new OpenLayers.Layer.WMS( "DM Solutions Demo", "http://www2.dmsolutions.ca/cgi-bin/mswms_gmap", {layers: "bathymetry,land_fn,park,drain_fn,drainage," + "prov_bound,fedlimit,rail,road,popplace", transparent: "true", format: "image/png" });
-		map.addLayers([ol_wms]);
-		map.addControl(new OpenLayers.Control.LayerSwitcher());
+		var osmarender = new OpenLayers.Layer.OSM(
+		                "OpenStreetMap (Tiles@Home)",
+		                "http://tah.openstreetmap.org/Tiles/tile/${z}/${x}/${y}.png"
+		            );
+		map.addLayer(osmarender);
 		map.setCenter(new OpenLayers.LonLat(0, 0), 6);
-		map.zoomToMapExtent();
+		map.zoomToMaxExtent();
 // }
